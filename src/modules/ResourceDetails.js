d04f7e35c1b17b4233b0ceeabf3d7f6f8b315e76
TuanLy <tuanth0010@gmail.com>
2021-12-06 23:39:22 +0700

    #0: first look detail

--- src/modules/ResourceDetails.js
+++ src/modules/ResourceDetails.js
@@ -6,11 +6,52 @@ import ResponsiveImage from '../components/responsive-image.jsx'
 import CallToAction from "../components/call-to-action.jsx"
 import './ResourceDetails.scss'
 import './RichTextArea.scss'
+
+
+
+const renderTags = (tags, type) => {
+
+	// if (typeof tags )
+	console.log(typeof tags, tags.length);
+	if (typeof (tags) === 'object' && !tags.length) {
+		let link = `/resources/${type}/${tags?.customFields?.title?.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-').replace(/--+/g, '-')}`
+		return (
+			<span className="d-inline-block cs-tag ps-rv">
+				{tags?.customFields?.title}
+				<Link to={link} target="_self" className="ps-as"><span className="sr-only">{tags?.customFields?.title}</span></Link>
+			</span>
+		)
+	}
+	return tags.map((tag, index) => {
+		let link = `/resources/${type}/${tag?.customFields?.title?.toLowerCase().replace(/[^a-zA-Z0-9]/g, '-').replace(/--+/g, '-')}`
+		return (
+			<span key={index} className="d-inline-block cs-tag ps-rv">
+				{tag?.customFields?.title}
+				<Link to={link} target="_self" className="ps-as"><span className="sr-only">{tag?.customFields?.title}</span></Link>
+			</span>
+		)
+	})
+}
+
+const RightCTA = ({rightCTAButton, rightCTAContent}) => {
+
+  return (
+    <div className="learn-more-cta bg-58 text-white">
+      <div className="d-table w-100">
+        <div className="d-table-cell align-middle text-center small-paragraph last-mb-none">
+					<div dangerouslySetInnerHTML={renderHTML(rightCTAContent)}></div>
+         <Link to={rightCTAButton.href} className="btn btn-white mb-0">Watch Now</Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 const ResourceDetails = ({ item, dynamicPageItem }) => {
 	let resource = dynamicPageItem.customFields;
 	item = item.customFields;
 
-	console.log('item', item);
+	console.log('item', resource);
 	return (
 		<section className="resource-details">
 			<div className="rich-text">
@@ -55,8 +96,60 @@ const ResourceDetails = ({ item, dynamicPageItem }) => {
 					}
 				</div>
 			</div>
+
+			<div className="space-100"></div>
+			
+			<div className="container">
+        <div className="d-flex flex-wrap">
+          <div className="cs-detail-cont-left content-ul beauty-ul">
+            <div className="cs-detail-inner last-mb-none">
+						<h1 className="h1">{resource.title}</h1>
+						{resource.subTitle &&
+							<p>{resource.subTitle}</p>
+						}
+						{resource.image &&
+							<div className="image">
+								<ResponsiveImage img={resource.image}
+									breaks={[{ w: 640, max: 640 }, { w: 780, max: 800 }, { w: 1200, max: 1920 }]} />
+							</div>
+						}
+            
+						<div className="content mt-35">
+							<div dangerouslySetInnerHTML={renderHTML(resource.textblob)}></div>
+						</div>
+            </div>
+          </div>
+          {/*  */}
+          <div className="cs-detail-cont-right">
+            <div className="small-paragraph cs-tag-wrap last-mb-none">
+              <h4>Categories</h4>
+              <p>
+                {renderTags(resource.resourceType, 'tag')}
+                {/* {renderTags(resource.resourceTopics, 'topic')} */}
+              </p>
+            </div>
+            <div className="small-paragraph cs-tag-wrap last-mb-none">
+              <h4>Topics</h4>
+              <p>
+                {renderTags(resource.resourceTopics, 'topic')}
+              </p>
+            </div>
+
+            {/*  */}
+            {/* <GetResourceForm /> */}
+            {/* recommend webinar */}
+            {/* <RecommendWebinar /> */}
+            <div className="space-80"></div>
+            {/* <CTA /> */}
+						<RightCTA rightCTAButton={resource.rightCTAButton} rightCTAContent={resource.rightCTAContent} />
+            
+          </div>
+        </div>
+      </div>
 		</section>
 	);
 }
 
 export default ResourceDetails;
+
+
